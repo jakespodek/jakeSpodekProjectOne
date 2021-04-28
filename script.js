63ee3eba8bf524d53d41b43b5cfba54d3c228edc
Jake Spodek <jake.spodek1@gmail.com>
2021-04-27 23:09:29 -0400

    added eventListener

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('test');
+
+// append submitted comments to page
+
+const app = {};
+
+app.colors = ['red', 'salmon', 'darkred', 'hotpink', 'mediumvioletred', 'tomato', 'orange', 'moccasin', 'khaki', 'yellow', 'lime', 'yelllowgreen', 'mediumaquamarine', 'green', 'blue', 'aqua', 'dodgerblue', 'midnightblue', 'teal', 'lavender', 'plum', 'mediumpurple', 'purple'];
+
+app.init = function () {
+    app.h1 = document.querySelector('h1');
+
+    app.h1.addEventListener('click', function () {
+        i = Math.floor(Math.random() * app.colors.length);
+        j = Math.floor(Math.random() * app.colors.length);
+        k = Math.floor(Math.random() * app.colors.length);
+        app.h1.style.color = app.colors[i];
+        app.h1.style.textShadow = `3px 2px ${k}px ${app.colors[j]}`
+    });   
+};
+
+app.init();
